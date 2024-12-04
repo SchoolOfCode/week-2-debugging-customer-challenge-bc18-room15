8ffd08f36b33ca543e3af23086a777ee566c0b05
Timothy Li <timothy93li@gmail.com>
2024-12-04 16:14:45 +0000

    resolved bug#1

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let score;
+let score = 0; // changed from let score;
 let playAgain = true;
 
 while (playAgain === false) 
-  const randomNumber = Math.floor(Math.random() * 100) + 1;
-  const userGuess = prompt("Guess a number between 1 and 10:");
+  const randomNumber = Math.floor(Math.random() * 10) + 1; // math.floor rounds down returns and interger less than or equal to DONE
+  const userGuess = prompt("Guess a number between 1 and 10:"); // meant to: We want a game where a player takes a guess at a number between 1 and 10
 
   if (userGuess = randomnumber) {
     alert("Congratulations! You guessed the correct number.");
@@ -19,3 +19,15 @@ while (playAgain === false)
     alert(`Game over. Your final score is ${randomNumber}.`);
   }
 }
+
+// Requirements:
+// - We want a game where a player takes a guess at a number between 1 and 10
+// bug1: changed randomNumber from 1 - 100, to 1 - 10 maximum DONE
+
+
+//- You should keep score, and if a player guesses correctly, you should add to the score
+
+
+
+
+// - You should ask the player if they want to continue the game, and keep playing until they refuse
